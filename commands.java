635272fd0bcdb72af6bca45ae1bda765a2339bb1
okancetin <okancetin@gmail.com>
2017-01-16 08:16:37 +0200

    Create commands.java

---
+++ commands.java
@@ -0,0 +1,8 @@
+curl -L https://github.com/docker/machine/releases/download/v0.9.0-rc2/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine &&
+  chmod +x /tmp/docker-machine &&
+  sudo cp /tmp/docker-machine /usr/local/bin/docker-machine
+  
+  
+  
+  
+  1
