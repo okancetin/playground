029f40d6229166641887129af2d7f0f85f553ad8
okancetin <okancetin@gmail.com>
2017-01-16 08:34:40 +0200

    Update commands.java

--- commands.java
+++ commands.java
@@ -5,5 +5,6 @@ curl -L https://github.com/docker/machine/releases/download/v0.9.0-rc2/docker-ma
   
   docker run -d ubuntu /bin/sh -c "while true; do echo hello world; sleep 1; done"
   
-  
-  1
+/etc/apt/apt.conf
+Acquire::http::proxy "http://OKANC:Telefon14@195.87.49.10:8080/";
+Acquire::https::proxy "https://OKANC:Telefon14@195.87.49.10:8080/";
